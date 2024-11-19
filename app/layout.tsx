bd14e9d67d1ac8b1448fac782f545092eb478e7d
Tushar Mehta <mehtatushar800@gmail.com>
2024-11-19 20:03:44 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
